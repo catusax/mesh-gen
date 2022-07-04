2becbb2b3b40195f3b12afa9cadb53102e15539a
catusax <root@coolrc.me>
2022-07-04 16:48:44 +0800

    :recycle:  test file package

--- scaffold/template/gitignore.go
+++ scaffold/template/gitignore.go
@@ -3,4 +3,8 @@ package template
 // GitIgnore is the .gitignore template used for new projects.
 var GitIgnore = `# don't commit the service binary to vcs
 {{.Service}}
+proto/*_test.go
+
+.idea
+.vscode
 `
--- scaffold/template/makefile.go
+++ scaffold/template/makefile.go
@@ -23,7 +23,7 @@ proto:
 	@cp handler/$(NAME)_test.go proto/$(NAME)_test.go || true
 	@protoc --proto_path=. -I${GOPATH}/src --go-grpc_out=. --go_out=:. --go-mesh-gen_out=. --go-mesh-gen_opt=mesh=traefik-mesh,namespace=$(NAMESPACE),port=$(PORT),name=$(NAME) proto/$(NAME).proto
 	@mv proto/$(NAME)_handler.go handler/$(NAME).go
-#	@mv proto/$(NAME)_test.go handler/$(NAME)_test.go
+	#@mv proto/$(NAME)_test.go handler/$(NAME)_test.go
 
 .PHONY: generate
 generate:
