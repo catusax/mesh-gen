c4e04c7e0864f9624df3f8e5becfcf58b8ea9208
catusax <root@coolrc.me>
2022-07-13 12:07:13 +0800

    :sparkles: improve docker build speed

--- scaffold/template/docker.go
+++ scaffold/template/docker.go
@@ -5,8 +5,13 @@ var Dockerfile = `FROM golang:alpine AS builder
 ENV CGO_ENABLED=0 GOOS=linux
 RUN apk --update --no-cache add ca-certificates gcc libtool make musl-dev protoc
 WORKDIR /go/src/{{.Service}}
+COPY go.mod .
+COPY go.sum .
+RUN --mount=type=cache,target=/go/pkg/mod go mod download
 COPY . .
-RUN --mount=type=cache,target=/root/.cache/go-build make tidy build
+RUN --mount=type=cache,target=/root/.cache/go-build \
+    --mount=type=cache,target=/go/pkg/mod \
+    make tidy build
 
 FROM scratch
 ENV CONTAINER=docker
